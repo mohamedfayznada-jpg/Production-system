2c61847a2b7569e92e7cd0e2ec26cfa56e43cd78
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-07-26 17:19:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import { API } from "./api.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
 
-    console.log("✅ Firebase Connected");
+    console.log("Firebase Ready");
+
+    await API.production.saveTest();
+
+    console.log("Production Saved");
 
 });
