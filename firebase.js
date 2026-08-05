82c5554275557ae73d3cf2cdbe4b29db62ac1481
mohamedfayznada-jpg <mohamedfayznada@gmail.com>
2026-08-05 15:56:33 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+// مفاتيح الربط الصحيحة لمشروع production-b0b2c
 const firebaseConfig = {
-    apiKey: "AIzaSyDwiSlMnznYKza8WTRX2CkRHvZZV_CE64A",
-    authDomain: "mes-core-v27.firebaseapp.com",
-    projectId: "mes-core-v27",
-    storageBucket: "mes-core-v27.firebasestorage.app",
-    messagingSenderId: "745936325795",
-    appId: "1:745936325795:web:09f5915cb116dbd3be0316"
+    apiKey: "AIzaSyC4GyboQgeYdNxwDyxkEowpXTsAT-CMFyg",
+    authDomain: "production-b0b2c.firebaseapp.com",
+    databaseURL: "https://production-b0b2c-default-rtdb.firebaseio.com",
+    projectId: "production-b0b2c",
+    storageBucket: "production-b0b2c.firebasestorage.app",
+    messagingSenderId: "628405148231",
+    appId: "1:628405148231:web:e277a8422324e358aaef62",
+    measurementId: "G-5KP3JMH9EZ"
 };
 
-
+// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
 
-// تهيئة قاعدة البيانات بدون تخزين محلي لضمان الاتصال اللحظي وكشف الأخطاء
+// تهيئة قاعدة البيانات للاتصال المباشر
 const db = getFirestore(app);
 
 export { db };
