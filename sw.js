e4c1f6afe49d9870121f530d90e8e80b360cb57a
mohamedfayznada-jpg <2.57306554e+08+mohamedfayznada-jpg@users.noreply.github.com>
2026-08-23 11:08:21 +0000

    Polish mobile typography icons and responsive layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this release marker whenever the PWA is published.
-const SW_RELEASE = '20260823-operations-5s-v1';
+const SW_RELEASE = '20260823-mobile-harmony-v1';
 
 self.addEventListener('install', (event) => {
     // Activate the new worker without waiting for all old tabs to close.
